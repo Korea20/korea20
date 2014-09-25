018205dd5165f525201211d9344cdc7a25bae7b4
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-09-25 21:24:04 +0200

    Initial commit. Nix expression.

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  pkgs = import <nixpkgs> {};
+  memoricidepkgs = import <memoricidepkgs> {};
+  stdenv = pkgs.stdenv;
+  elixir = import ./elixir.nix {};
+in
+{
+  developmentEnv = stdenv.mkDerivation rec {
+    name = "developmentEnv";
+    version = "nightly";
+    src = ./.;
+    buildInputs = [
+      memoricidepkgs.elixir
+      pkgs.erlang
+      pkgs.rebar
+    ];
+  };
+}
